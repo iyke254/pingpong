6feac010a358adf473ce428ace660433a2bb677b
iyke <nderiony95@gmail.com>
2017-05-05 14:51:05 +0300

    add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 var game = function(png) {
   for (var i = 1; i <= png; i++) {
-    if (i % 3 === 0) {
-      $("#result").append("<li>ping</li>");
+    if (i % 15 === 0) {
+      $("#result").append("<li class='pgr'>pingpong</li>");
+    } else if (i % 5 === 0) {
+      $("#result").append("<li class='pgr'>pong</li>");
+    } else if (i % 3 === 0) {
+      $("#result").append("<li class='pgr'>ping</li>");
+    } else {
+      $("#result").append("<li class='pgr'>"+String(i)+"</li>");
+      }
     }
   }
-}
+
 //user interface
 $(document).ready(function() {
   $("form#game").submit(function(event) {
+    $("li.pgr").remove();
     event.preventDefault();
     var png = parseInt($("input#png").val());
     game(png);
