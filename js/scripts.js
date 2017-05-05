11c42050f8a712c24ec57eddde50be72b8d90d7b
iyke <nderiony95@gmail.com>
2017-05-05 12:23:08 +0300

    add text and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-//Write your code here
+var game = function(png) {
+  for (var i = 1; i <= png; i++) {
+    if (i % 3 === 0) {
+      $("#result").append("<li>ping</li>");
+    }
+  }
+}
+//user interface
+$(document).ready(function() {
+  $("form#game").submit(function(event) {
+    event.preventDefault();
+    var png = parseInt($("input#png").val());
+    game(png);
+
+  });
+});
